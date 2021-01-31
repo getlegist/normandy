d2dc5cd14cf878f288f31d28dbc66f6e684819fa
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-31 14:19:30 -0500

    🚧 App module signatures

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { UserModule } from './user/user.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user/user'
+import { Policy } from './policy/policy'
+import { Index } from './index'
+import { LegislationEvent } from './events/legislation/legislation-event'
+import { NamedEntity } from './entities/named-entity'
+import { Category } from './category/category'
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		UserModule,
+		GraphQLModule.forRoot({
+			playground: true,
+			introspection: true,
+			autoSchemaFile: join(process.cwd() + 'src/schema.gql'),
+			sortSchema: true,
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			url: 'postgres://postgres@localhost:4445/postgres',
+			entities: [User, Policy, Index, LegislationEvent, NamedEntity, Category],
+		}),
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
