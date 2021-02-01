8187956669ebef427cec5000d8333aa748a07d0f
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-01 12:49:42 -0500

    Abstract out AppModule class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { UserModule } from './user/user.module'
@@ -9,23 +9,47 @@ import { Index } from './index'
 import { LegislationEvent } from './events/legislation/legislation-event'
 import { NamedEntity } from './entities/named-entity'
 import { Category } from './category/category'
+import { PolicyModule } from './policy/policy.module'
+import { CategoryModule } from './category/category.module'
+import { LegislationEventModule } from './events/legislation/legislation-event.module'
+import { IndexModule } from './index/index.module'
+import { NamedEntityModule } from './entities/named-entity.module'
 
-@Module({
-	imports: [
-		UserModule,
-		GraphQLModule.forRoot({
-			playground: true,
-			introspection: true,
-			autoSchemaFile: join(process.cwd() + 'src/schema.gql'),
-			sortSchema: true,
-		}),
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			url: 'postgres://postgres@localhost:4445/postgres',
-			entities: [User, Policy, Index, LegislationEvent, NamedEntity, Category],
-		}),
-	],
-	controllers: [],
-	providers: [],
-})
-export class AppModule {}
+export const AppModule = (dbURL: string): any => {
+	@Global()
+	@Module({
+		imports: [
+			UserModule,
+			TypeOrmModule.forRoot({
+				type: 'postgres',
+				url: dbURL,
+				entities: [
+					User,
+					Policy,
+					Index,
+					LegislationEvent,
+					NamedEntity,
+					Category,
+				],
+				synchronize: true,
+			}),
+			GraphQLModule.forRoot({
+				playground: true,
+
+				introspection: true,
+				autoSchemaFile: join(process.cwd() + 'src/schema.gql'),
+				sortSchema: true,
+			}),
+			PolicyModule,
+			CategoryModule,
+			LegislationEventModule,
+			IndexModule,
+			NamedEntityModule,
+		],
+		controllers: [],
+		providers: [],
+	})
+	abstract class BaseAppModule {}
+
+	return BaseAppModule
+}
