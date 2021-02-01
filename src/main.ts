e1f344f398c3461323c635143d9b4cdeb987c276
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-01 12:48:50 -0500

    make bootstrap command instance-only for easier packaging

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { INestApplication } from '@nestjs/common'
+import * as dotenv from 'dotenv'
 
 export class Normandy {
-	public static async bootstrap(port?: number) {
-		const app = await NestFactory.create(AppModule)
+	private app: INestApplication
+
+	constructor(private dbURL: string) {}
+
+	public async bootstrap(port?: number) {
+		const app = await NestFactory.create(AppModule(this.dbURL))
 		await app.listen(port ?? 3000)
+		this.app = app
 	}
 }
 
 ;(async () => {
-	await Normandy.bootstrap()
+	dotenv.config()
+	const instance = new Normandy(process.env.db)
+	await instance.bootstrap(3000)
 })()
