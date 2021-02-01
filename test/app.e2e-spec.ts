3ff07b79cc5c47355773aa91b5cb651340729641
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-01 12:53:28 -0500

    update e2e spec to add in new abstraction

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { config } from 'dotenv'
 
 describe('AppController (e2e)', () => {
 	let app: INestApplication
 
 	beforeEach(async () => {
+		config()
 		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [AppModule],
+			imports: [AppModule(process.env.db)],
 		}).compile()
 
 		app = moduleFixture.createNestApplication()
