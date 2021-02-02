4275c985259eb9cd16b7b04915c17b54e1e64e00
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:18:10 -0500

    create namedEntity module implementation

--- src/entities/resolvers/named-entity.resolver.ts
+++ src/entities/resolvers/named-entity.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { NamedEntityService } from '../service/named-entity.service'
+import { BaseEntityResolver } from '../../shared/graphql'
+import { NamedEntity } from '../named-entity'
+import { CreateNamedEntityInput } from './inputs/CreateNamedEntity.input'
 
 @Resolver()
-export class NamedEntityResolver {}
+export class NamedEntityResolver extends BaseEntityResolver(NamedEntity) {
+	constructor(private namedEntityService: NamedEntityService) {
+		super(namedEntityService)
+	}
+
+	@Mutation(() => NamedEntity)
+	createEmptyNamedEntity() {
+		return this.namedEntityService.createEmptyNamedEntity()
+	}
+
+	@Mutation(() => NamedEntity)
+	createNamedEntity(@Args('input') input: CreateNamedEntityInput) {
+		return this.namedEntityService.createNamedEntity(input)
+	}
+}
--- src/entities/service/named-entity.service.ts
+++ src/entities/service/named-entity.service.ts
@@ -1,17 +1,79 @@
 import { Injectable } from '@nestjs/common'
-import { BaseEntityService } from '../../shared/database'
+import {
+	assignCollectionsToEntity,
+	BaseEntityService,
+	getCollection,
+} from '../../shared/database'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { NamedEntity } from '../named-entity'
 import { EntityOrStrings } from '../../shared/types'
 import { Index } from '../../index'
+import { CreateNamedEntityInput } from './inputs/CreateNamedEntity.input'
+import { Policy } from '../../policy/policy'
+import { LegislationEvent } from '../../events/legislation/legislation-event'
+import { Category } from '../../category/category'
 
 @Injectable()
 export class NamedEntityService extends BaseEntityService(NamedEntity) {
 	constructor(
 		@InjectRepository(NamedEntity)
-		private entityRepository: Repository<NamedEntity>
+		private entityRepository: Repository<NamedEntity>,
+		@InjectRepository(Index)
+		private indexRepository: Repository<Index>,
+		@InjectRepository(Category)
+		private categoryRepository: Repository<Category>,
+		@InjectRepository(LegislationEvent)
+		private legislationEventRepository: Repository<LegislationEvent>,
+		@InjectRepository(Policy)
+		private policyRepository: Repository<Policy>
 	) {
 		super(entityRepository)
+	}
+
+	public async createEmptyNamedEntity(): Promise<NamedEntity> {
+		const entity = this.entityRepository.create()
+		entity.categories = []
+		entity.legislationEvents = []
+		entity.indices = []
+		entity.policies = []
+		entity.title = ''
+		entity.description = ''
+
+		await this.entityRepository.save(entity)
+
+		return entity
+	}
+
+	public async createNamedEntity({
+		categories,
+		legislationEvents,
+		indices,
+		policies,
+		title,
+		description,
+	}: CreateNamedEntityInput): Promise<NamedEntity> {
+		const entity = this.entityRepository.create()
+
+		entity.description = description
+		entity.title = title
+
+		await assignCollectionsToEntity(
+			[
+				[
+					'legislationEvents',
+					legislationEvents,
+					this.legislationEventRepository,
+				],
+				['policies', policies, this.policyRepository],
+				['indices', indices, this.indexRepository],
+				['categories', categories, this.categoryRepository],
+			],
+			entity
+		)
+
+		await this.entityRepository.save(entity)
+
+		return entity
 	}
 }
