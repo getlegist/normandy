87c8da4e9851797bd6bc597258240381ffa138c3
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:17:07 -0500

    update namedEntity imports

--- src/entities/named-entity.module.ts
+++ src/entities/named-entity.module.ts
@@ -3,9 +3,21 @@ import { NamedEntityService } from './service/named-entity.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { NamedEntity } from './named-entity'
 import { NamedEntityResolver } from './resolvers/named-entity.resolver'
+import { Category } from '../category/category'
+import { Policy } from '../policy/policy'
+import { LegislationEvent } from '../events/legislation/legislation-event'
+import { Index } from '../index'
 
 @Module({
-	imports: [TypeOrmModule.forFeature([NamedEntity])],
+	imports: [
+		TypeOrmModule.forFeature([
+			NamedEntity,
+			Index,
+			Category,
+			Policy,
+			LegislationEvent,
+		]),
+	],
 	providers: [NamedEntityService, NamedEntityResolver],
 	exports: [NamedEntityService],
 })
