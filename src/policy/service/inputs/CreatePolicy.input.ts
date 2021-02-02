1ab86a8cdd1cd4dc177a61a3bff991c2b2db6989
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:17:25 -0500

    create CreatePolicy input class

--- src/policy/service/inputs/CreatePolicy.input.ts
+++ src/policy/service/inputs/CreatePolicy.input.ts
@@ -1,11 +1,12 @@
 import { Category } from '../../../category/category'
 import { LegislationEvent } from '../../../events/legislation/legislation-event'
 import { NamedEntity } from '../../../entities/named-entity'
+import { EntityOrStrings } from '../../../shared/types'
 
 export interface CreatePolicyInput {
-	categories?: string[] | Category[]
-	legislationEvents?: string[] | LegislationEvent[]
-	namedEntities?: string[] | NamedEntity[]
+	categories?: EntityOrStrings<Category>
+	legislationEvents?: EntityOrStrings<LegislationEvent>
+	namedEntities?: EntityOrStrings<NamedEntity>
 	title: string
 	description: string
 }
