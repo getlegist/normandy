14adea90fb147db683c93c17d2787f57aade3462
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:06:50 -0500

    create LegislationEvent module

--- src/events/legislation/resolvers/legislation-event.resolver.ts
+++ src/events/legislation/resolvers/legislation-event.resolver.ts
@@ -1,7 +1,8 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { BaseEntityResolver } from '../../../shared/graphql'
 import { LegislationEvent } from '../legislation-event'
 import { LegislationEventService } from '../service/legislation-event.service'
+import { CreateLegislationEventInput } from './inputs/CreateLegislationEvent.input'
 
 @Resolver()
 export class LegislationEventResolver extends BaseEntityResolver(
@@ -9,5 +10,15 @@ export class LegislationEventResolver extends BaseEntityResolver(
 ) {
 	constructor(private legislationEventService: LegislationEventService) {
 		super(legislationEventService)
+	}
+
+	@Mutation(() => LegislationEvent)
+	createLegislationEvent(@Args('input') input: CreateLegislationEventInput) {
+		return this.legislationEventService.createLegislationEvent(input)
+	}
+
+	@Mutation(() => LegislationEvent)
+	createEmptyLegislationEvent() {
+		return this.legislationEventService.createEmptyLegislationEvent()
 	}
 }
--- src/events/legislation/service/legislation-event.service.ts
+++ src/events/legislation/service/legislation-event.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common'
-import { BaseEntityService } from '../../../shared/database'
+import {
+	assignCollectionsToEntity,
+	BaseEntityService,
+	getCollection,
+} from '../../../shared/database'
 import { LegislationEvent } from '../legislation-event'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { CreateLegislationEventInput } from './inputs/CreateLegislationEvent.input'
 
 @Injectable()
 export class LegislationEventService extends BaseEntityService(
@@ -13,5 +18,51 @@ export class LegislationEventService extends BaseEntityService(
 		private eventRepository: Repository<LegislationEvent>
 	) {
 		super(eventRepository)
+	}
+
+	public async createEmptyLegislationEvent(): Promise<LegislationEvent> {
+		const entity = this.eventRepository.create()
+		entity.description = ''
+		entity.text = ''
+		entity.original = ''
+
+		entity.policies = []
+		entity.indices = []
+		entity.entities = []
+		entity.categories = []
+
+		await this.eventRepository.save(entity)
+
+		return entity
+	}
+
+	public async createLegislationEvent({
+		description,
+		text,
+		original,
+		policies,
+		indices,
+		entities,
+		categories,
+	}: CreateLegislationEventInput): Promise<LegislationEvent> {
+		const entity = this.eventRepository.create()
+
+		entity.original = original
+		entity.text = text
+		entity.description = description
+
+		await assignCollectionsToEntity(
+			[
+				['entities', entities, this.namedEntityRepository],
+				['policies', policies, this.policyRepository],
+				['indices', indices, this.indexRepository],
+				['categories', categories, this.categoryRepository],
+			],
+			entity
+		)
+
+		await this.eventRepository.save(entity)
+
+		return entity
 	}
 }
