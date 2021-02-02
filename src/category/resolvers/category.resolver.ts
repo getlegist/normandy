7cb65e3a54a17d5544a05e002db9777a2c3888e7
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:07:18 -0500

    create category module implementation

--- src/category/resolvers/category.resolver.ts
+++ src/category/resolvers/category.resolver.ts
@@ -1,11 +1,22 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { BaseEntityResolver } from '../../shared/graphql'
 import { Category } from '../category'
 import { CategoryService } from '../service/category.service'
+import { CreateCategoryInput } from './inputs/CreateCategory.input'
 
 @Resolver()
 export class CategoryResolver extends BaseEntityResolver(Category) {
 	constructor(private categoryService: CategoryService) {
 		super(categoryService)
+	}
+
+	@Mutation(() => Category)
+	createEmptyCategory() {
+		return this.categoryService.createEmptyCategory()
+	}
+
+	@Mutation(() => Category)
+	createCategory(@Args('input') input: CreateCategoryInput) {
+		return this.categoryService.createCategory(input)
 	}
 }
--- src/category/service/category.service.ts
+++ src/category/service/category.service.ts
@@ -1,15 +1,66 @@
 import { Injectable } from '@nestjs/common'
-import { BaseEntityService } from '../../shared/database'
+import {
+	assignCollectionsToEntity,
+	BaseEntityService,
+	getCollection,
+} from '../../shared/database'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Category } from '../category'
+import { CreateCategoryInput } from './inputs/CreateCategory.input'
+import { NamedEntity } from '../../entities/named-entity'
+import { LegislationEvent } from '../../events/legislation/legislation-event'
+import { Policy } from '../../policy/policy'
 
 @Injectable()
 export class CategoryService extends BaseEntityService(Category) {
 	constructor(
 		@InjectRepository(Category)
-		private categoryRepository: Repository<Category>
+		private categoryRepository: Repository<Category>,
+		@InjectRepository(NamedEntity)
+		private namedEntityRepository: Repository<NamedEntity>,
+		@InjectRepository(LegislationEvent)
+		private legislationEventRepository: Repository<LegislationEvent>,
+		@InjectRepository(Policy)
+		private policyRepository: Repository<Policy>
 	) {
 		super(categoryRepository)
+	}
+
+	public async createEmptyCategory(): Promise<Category> {
+		const entity = this.categoryRepository.create()
+
+		entity.entities = []
+		entity.legislationEvents = []
+		entity.policies = []
+		entity.description = ''
+		entity.title = ''
+
+		await this.categoryRepository.save(entity)
+		return entity
+	}
+
+	public async createCategory(input: CreateCategoryInput): Promise<Category> {
+		const entity = this.categoryRepository.create()
+
+		entity.description = input.description
+		entity.title = input.title
+
+		await assignCollectionsToEntity(
+			[
+				[
+					'legislationEvents',
+					input.legislationEvents,
+					this.legislationEventRepository,
+				],
+				['entities', input.entities, this.namedEntityRepository],
+				['policies', input.policies, this.policyRepository],
+			],
+			entity
+		)
+
+		await this.categoryRepository.save(entity)
+
+		return entity
 	}
 }
