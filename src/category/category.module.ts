5c017fbc8fa9c1a0f2b244f9909344e1f11ae9c1
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:07:29 -0500

    update imports for category module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,10 +3,14 @@ import { CategoryService } from './service/category.service'
 import { CategoryResolver } from './resolvers/category.resolver'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Category } from './category'
-import { Index } from '../index'
+import { NamedEntity } from '../entities/named-entity'
+import { LegislationEvent } from '../events/legislation/legislation-event'
+import { Policy } from '../policy/policy'
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Category])],
+	imports: [
+		TypeOrmModule.forFeature([Category, NamedEntity, LegislationEvent, Policy]),
+	],
 	providers: [CategoryService, CategoryResolver],
 	exports: [CategoryService],
 })
