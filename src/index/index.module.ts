881a926a5ed8ccaba265a0af409ba104ac86c936
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-02 17:06:59 -0500

    update imports for index module

--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { IndexService } from './service/index.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Index } from './index'
-import { IndexResolver } from './resolvers/index.resolver';
+import { IndexResolver } from './resolvers/index.resolver'
+import { LegislationEvent } from '../events/legislation/legislation-event'
+import { NamedEntity } from '../entities/named-entity'
 
 @Module({
 	providers: [IndexService, IndexResolver],
-	imports: [TypeOrmModule.forFeature([Index])],
+	imports: [TypeOrmModule.forFeature([Index, LegislationEvent, NamedEntity])],
 })
 export class IndexModule {}
