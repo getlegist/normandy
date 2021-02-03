3a48bb7e885f286007904dbf2e095797737c8b2b
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-03 09:44:26 -0500

    ✨ Update service for Indedx

--- src/index/service/index.service.ts
+++ src/index/service/index.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common'
-import { BaseEntityService, getCollection } from '../../shared/database'
+import {
+	assignStaticOptional,
+	BaseEntityService,
+	completeMergeEntity,
+	completeUpdateEntity,
+	constructRelations,
+	getCollection,
+} from '../../shared/database'
 import { Index } from '../index'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { Repository, SaveOptions } from 'typeorm'
 import { CreateIndexInput } from './inputs/CreateIndex.input'
 import { LegislationEvent } from '../../events/legislation/legislation-event'
 import { NamedEntity } from '../../entities/named-entity'
+import { CreatePolicyInput } from '../../policy/service/inputs/CreatePolicy.input'
+import { Policy } from '../../policy/policy'
 
 @Injectable()
 export class IndexService extends BaseEntityService(Index) {
@@ -41,6 +50,46 @@ export class IndexService extends BaseEntityService(Index) {
 
 		entity.start = input.start
 		entity.end = input.end
+
+		await this.indexRepository.save(entity)
+
+		return entity
+	}
+
+	public async updateIndex(
+		id: string,
+		input: Partial<CreateIndexInput>,
+		saveOptions?: SaveOptions
+	): Promise<Index> {
+		const entity = await this.indexRepository.findOneOrFail(id, {
+			relations: constructRelations(input, ['legislationEvent', 'entity']),
+		})
+		const assign = assignStaticOptional(entity, input)
+
+		assign('start')
+		assign('end')
+
+		if (input.entity !== undefined) {
+			if (typeof input.entity === 'string') {
+				const namedEntity = await this.namedEntityRepository.findOneOrFail(
+					input.entity
+				)
+				entity.entity = namedEntity
+			} else {
+				entity.entity = input.entity
+			}
+		}
+
+		if (input.legislationEvent !== undefined) {
+			if (typeof input.legislationEvent === 'string') {
+				const legislationEvent = await this.legislationEventRepository.findOneOrFail(
+					input.legislationEvent
+				)
+				entity.legislationEvent = legislationEvent
+			} else {
+				entity.legislationEvent = input.legislationEvent
+			}
+		}
 
 		await this.indexRepository.save(entity)
 
